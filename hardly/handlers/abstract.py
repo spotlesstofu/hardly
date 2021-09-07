9a0b95c3fd9dbf5be6a9617c2aab89c76d2fe278
Jiri Popelka <jpopelka@redhat.com>
2021-09-07 19:57:45 +0200

    Remove unused tasks

--- hardly/handlers/abstract.py
+++ hardly/handlers/abstract.py
@@ -8,7 +8,4 @@ logger = getLogger(__name__)
 
 
 class TaskName(str, Enum):
-    copr_build_start = "task.run_copr_build_start_handler"
-    copr_build_end = "task.run_copr_build_end_handler"
-    copr_build = "task.run_copr_build_handler"
     dist_git_pr = "task.run_dist_git_pr_handler"
